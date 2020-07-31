d4e64af2f2023869b8078ecc99cd6441d16e02aa
Denis <d@f12.biz>
2020-07-31 12:24:28 -0700

    Demo: Cache Busting.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
 
 export default {
   debug: true,
@@ -14,19 +15,24 @@ export default {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    // Hash the files using MD5 so that their names change when the content changes.
+    new WebpackMd5Hash(),
+
     //Use CommonChunkPlugin to create a separate bundle
     // of vendor libraries so that they're cached separately.
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor'
     }),
+
     // Create HTML file that includes reference to build JS.
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       inject: true
     }),
+
     // Eliminate duplicate packages when generating bandle
     new webpack.optimize.DedupePlugin(),
     // Minify js
