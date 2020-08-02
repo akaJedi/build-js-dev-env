53e2a22c5e182e1134e58159fcf9b678a8a875e7
Denis <d@f12.biz>
2020-08-02 07:43:45 -0700

    Demo: Automated API Deploy to heroku

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/':'/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/':'https://desolate-coast-18034.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
